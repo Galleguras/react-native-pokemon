501623fed30c3e4a4a123703453b3c37001b2331
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2022-09-09 13:38:05 +0200

    Pantalla de Mi cuenta

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  userName: "xagustin93",
+  password: "123456",
+};
+
+export const userDetails = {
+  userName: "xagustin93",
+  firstName: "Agustin",
+  lastName: "Navarro Galdon",
+  email: "xAgustin93@gmail.com",
+};
